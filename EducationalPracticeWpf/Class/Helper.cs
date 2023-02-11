9c49f74ef0a85de1591d82321420de49ae28c06b
MrPopsil-edu <125121741+MrPopsil-edu@users.noreply.github.com>
2023-02-11 12:02:37 +0300

    Add files via upload

---
+++ EducationalPracticeWpf/Class/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace EducationalPracticeWpf.Class
+{
+    public static class Helper
+    {
+        public static EducationalPracticeWpfEntities BD;
+        public static Users userActive = new Users();
+        static Helper()
+        {
+            try
+            {
+                BD = new EducationalPracticeWpfEntities();
+                if (BD.Database.Exists() == false)
+                {
+                    MessageBox.Show("БД не найдена!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    BD = null;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Не найдена BD", "Ошибка открытия BD", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
